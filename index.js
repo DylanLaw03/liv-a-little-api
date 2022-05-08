f1782c7974bdf35afbdf03d162a0145cea7708c0
Dylan <dylan.lawrence@uvm.edu>
2022-05-08 00:20:28 -0400

    ability to upload pic

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get('/', async (req, res) => {
 })
 
 app.post('/uploadPost', async (req, res) => {
+  console.log(req.body.postImg)
   cloudinary.uploader.upload(req.body.postImg);
 })
 const port = process.env.PORT || 5000;
